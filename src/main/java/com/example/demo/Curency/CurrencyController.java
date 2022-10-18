818b430efc9d41e8f3c7f055e74ac188c862785e
AkePunchai <akepunchai@gmail.com>
2022-10-18 14:07:03 +0700

    comment / path with return "Home"

--- src/main/java/com/example/demo/Curency/CurrencyController.java
+++ src/main/java/com/example/demo/Curency/CurrencyController.java
@@ -21,8 +21,8 @@ public class CurrencyController {  private final CurrencyService currencyService
         return currencyService.getCurrency();
 
     }
-    @GetMapping(value = "/")
-    public String home(){
-        return "Home";
-    }
+//    @GetMapping(value = "/")
+//    public String home(){
+//        return "Home";
+//    }
 }
